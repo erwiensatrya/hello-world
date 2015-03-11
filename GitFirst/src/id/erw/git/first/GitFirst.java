df7b0310b4149c478f0a8cbd5ac613325fd462e7
Erwien Satrya <erwien.satrya@live.com>
2015-03-11 18:35:19 +0700

    test

--- GitFirst/src/id/erw/git/first/GitFirst.java
+++ GitFirst/src/id/erw/git/first/GitFirst.java
@@ -7,7 +7,7 @@ public class GitFirst {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Git is cool.");
-		System.out.println("how to make changes");
+		System.out.println("how to make changes");		
 	}
 
 }
